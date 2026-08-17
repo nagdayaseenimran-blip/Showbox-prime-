eb07d0fe7a07daed8d7084e9e31b451855e8bfc2
nagdayaseenimran-blip <nagdayaseenimran@gmail.com>
2026-08-18 02:11:32 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,143 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const FlickBizzApp());
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: DashboardScreen()));
 }
 
-class FlickBizzApp extends StatelessWidget {
-  const FlickBizzApp({super.key});
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'FlickBizz Media',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF121212),
-        primaryColor: const Color(0xFF00F0FF),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xFF1E1E1E),
-          selectedItemColor: Color(0xFF00F0FF),
-          unselectedItemColor: Colors.grey,
+    final List<Map<String, dynamic>> menuItems = [
+      {'title': 'Movies', 'icon': Icons.movie, 'url': 'https://www.flickbizz.com.pk/movies', 'color': Colors.red},
+      {'title': 'Music', 'icon': Icons.music_note, 'url': 'https://www.flickbizz.com.pk/music', 'color': Colors.blue},
+      {'title': 'Web TV', 'icon': Icons.live_tv, 'url': 'https://www.flickbizz.com.pk/webtv', 'color': Colors.green},
+      {'title': 'Videos', 'icon': Icons.video_library, 'url': 'https://www.flickbizz.com.pk/videos', 'color': Colors.orange},
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF121212),
+      appBar: AppBar(title: const Text("FlickBizz Hub"), backgroundColor: Colors.transparent),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 15, mainAxisSpacing: 15),
+          itemCount: menuItems.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => WebPlayer(url: menuItems[index]['url'], title: menuItems[index]['title']))),
+              child: Container(
+                decoration: BoxDecoration(color: menuItems[index]['color'], borderRadius: BorderRadius.circular(20)),
+                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Icon(menuItems[index]['icon'], size: 50, color: Colors.white),
+                  const SizedBox(height: 10),
+                  Text(menuItems[index]['title'], style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+                ]),
+              ),
+            );
+          },
         ),
       ),
-      home: const MainMediaScreen(),
     );
   }
 }
 
-class MainMediaScreen extends StatefulWidget {
-  const MainMediaScreen({super.key});
-
-  @override
-  State<MainMediaScreen> createState() => _MainMediaScreenState();
-}
-
-class _MainMediaScreenState extends State<MainMediaScreen> {
-  int _currentIndex = 0;
-
-  final List<Map<String, dynamic>> _navItems = [
-    {
-      'title': 'Movies',
-      'icon': Icons.movie_outlined,
-      'activeIcon': Icons.movie,
-      'url': 'https://www.flickbizz.com.pk/movies',
-    },
-    {
-      'title': 'Music',
-      'icon': Icons.music_note_outlined,
-      'activeIcon': Icons.music_note,
-      'url': 'https://www.flickbizz.com.pk/music',
-    },
-    {
-      'title': 'Web TV',
-      'icon': Icons.live_tv_outlined,
-      'activeIcon': Icons.live_tv,
-      'url': 'https://www.flickbizz.com.pk/webtv',
-    },
-    {
-      'title': 'Videos',
-      'icon': Icons.video_library_outlined,
-      'activeIcon': Icons.video_library,
-      'url': 'https://www.flickbizz.com.pk/videos',
-    },
-  ];
+class WebPlayer extends StatelessWidget {
+  final String url;
+  final String title;
+  const WebPlayer({super.key, required this.url, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF1E1E1E),
-        title: Text(
-          _navItems[_currentIndex]['title'],
-          style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        elevation: 0,
-        centerTitle: true,
+      appBar: AppBar(title: Text(title)),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: WebUri(url)),
+        initialSettings: InAppWebViewSettings(javaScriptEnabled: true, allowsInlineMediaPlayback: true),
       ),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _navItems.map((item) {
-          return WebViewTab(url: item['url']);
-        }).toList(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: _navItems.map((item) {
-          return BottomNavigationBarItem(
-            icon: Icon(item['icon']),
-            activeIcon: Icon(item['activeIcon']),
-            label: item['title'],
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
-
-class WebViewTab extends StatefulWidget {
-  final String url;
-  const WebViewTab({super.key, required this.url});
-
-  @override
-  State<WebViewTab> createState() => _WebViewTabState();
-}
-
-class _WebViewTabState extends State<WebViewTab> {
-  double _progress = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        InAppWebView(
-          initialUrlRequest: URLRequest(url: WebUri(widget.url)),
-          initialSettings: InAppWebViewSettings(
-            javaScriptEnabled: true,
-            mediaPlaybackRequiresUserGesture: false,
-            allowsInlineMediaPlayback: true,
-            useHybridComposition: true,
-          ),
-          onProgressChanged: (controller, progress) {
-            setState(() {
-              _progress = progress / 100;
-            });
-          },
-        ),
-        if (_progress < 1.0)
-          LinearProgressIndicator(
-            value: _progress,
-            color: const Color(0xFF00F0FF),
-            backgroundColor: Colors.black26,
-          ),
-      ],
     );
   }
 }
